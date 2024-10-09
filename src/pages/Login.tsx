0ba6f302a9fe49aace83900dbe0d31893424c0d9
SeungHyun CHA <cjhms002@g.yju.ac.kr>
2024-10-09 23:58:08 +0900

    💡 feat: Design Login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -8,14 +8,14 @@ function Login() {
           <h3 className="login-title">Login</h3>
         </div>
         <div className="google-login"></div>
-        {/* <div className="w-100">
+        <div className="w-100">
           <input type="text" className="email" placeholder="Email@exam.com" />
           <input type="text" className="passwd" placeholder="Password" />
         </div>
         <div>
           <label className="sign-up">Sign Up</label>
           <button className="login-btn">Login</button>
-        </div> */}
+        </div>
       </div>
     </div>
   );
