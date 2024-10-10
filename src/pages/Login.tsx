f8fce9a95d2e56e38fc1b3928251f0e497abf8ac
SeungHyun CHA <cjhms002@g.yju.ac.kr>
2024-10-10 12:33:15 +0900

    🐛 fix: google login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,21 +1,41 @@
 import "../styles/login.css";
+import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
+import { jwtDecode } from "jwt-decode";
 
 function Login() {
+  const handleLoginSuccess = (response: any) => {
+    const decoded: any = jwtDecode(response.credential);
+    console.log("Google User:", decoded);
+    // 필요한 사용자 정보 사용 가능 (예: 이름, 이메일 등)
+  };
+
+  const handleLoginError = () => {
+    console.log("Login Failed");
+  };
+
   return (
     <div className="login">
       <div className="container">
         <div className="w-100">
           <h3 className="login-title">Login</h3>
         </div>
-        <div className="google-login"></div>
-        <div className="w-100">
+        <hr />
+        <div className="google-login">
+          <GoogleOAuthProvider clientId="YOUR_GOOGLE_CLIENT_ID">
+            <GoogleLogin
+              onSuccess={handleLoginSuccess}
+              onError={handleLoginError}
+            />
+          </GoogleOAuthProvider>
+        </div>
+        {/* <div className="w-100">
           <input type="text" className="email" placeholder="Email@exam.com" />
           <input type="text" className="passwd" placeholder="Password" />
         </div>
         <div>
           <label className="sign-up">Sign Up</label>
           <button className="login-btn">Login</button>
-        </div>
+        </div> */}
       </div>
     </div>
   );
