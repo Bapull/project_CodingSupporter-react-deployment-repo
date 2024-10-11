c54267f4426ae8965c38cbb773d89b7d1bbbda89
MunSeongyun <borygashill608@gmail.com>
2024-10-11 17:42:13 +0900

    ✅ Test : Google Login Test

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,9 +1,40 @@
+import { useEffect, useState } from "react";
 import "../styles/user.css";
 
+type User = {
+  googleId:string
+  id:number
+  name:string
+  position:number
+  profilePicture:string
+  useLanguage:"[]"
+}
 function User() {
+  const [user, setUser] = useState<User | null>(null)
+  useEffect(()=>{
+    fetch('https://localhost:3000/auth/status',{
+      method:"GET",
+      credentials:'include'
+    })
+    .then(res=>res.json())
+    .then(data=>setUser(data))
+  },[])
+
+  const logout = () => {
+    fetch('https://localhost:3000/auth/logout',{
+      method:"GET",
+      credentials:'include'
+    }).then(()=>{window.location.href='https://localhost:5173'})
+  }
   return (
     <div className="user">
-      <div className="container">User</div>
+      <div className="container">
+        <h2>name:{user?.name}</h2>
+        <img src={user?.profilePicture} alt="프로필사진" />
+        <p>역할: {user?.position === 1 ? '튜터' : '튜티'}</p>
+        <p>사용 언어: {user?.useLanguage}</p>
+        <button onClick={logout}>로그아웃</button>
+      </div>
     </div>
   );
 }
