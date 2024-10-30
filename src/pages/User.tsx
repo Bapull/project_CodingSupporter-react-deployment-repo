aa8064b6d434c949b0df2a6be42dbb19c114efa2
SeungHyun CHA <cjhms002@g.yju.ac.kr>
2024-10-30 19:55:55 +0900

    ✅ test: 로그인 시 화면 구성 수정 중

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -42,6 +42,7 @@ function User() {
       dispatch(clearUser()); // 유저 정보를 dispatch를 이용해서 리덕스 스토어에서 삭제
       window.location.href = "https://localhost:5173";
     });
+    window.location.replace("/"); // 홈 화면으로 사라져랏 !
   };
 
   return (
