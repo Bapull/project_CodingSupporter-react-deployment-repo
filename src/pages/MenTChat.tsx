7564acf394bc7341de701d0188e536b08da98c34
SeungHyun CHA <cjhms002@g.yju.ac.kr>
2024-12-03 02:47:52 +0900

    🎨 style: add mentchat container

--- src/pages/MenTChat.tsx
+++ src/pages/MenTChat.tsx
@@ -1,9 +1,18 @@
 import "../styles/menTChat.css";
+import { useLocation } from "react-router-dom";
+import IncorrectNote from "../components/IncorrectNote";
 
 function MenTChat() {
+  const location = useLocation();
+
   return (
-    <div className="menTChat">
-      <div className="container">MentChat</div>
+    <div className="mentor-chat">
+      <div className="wrapper">
+        <div className="mentor-chat-content-left">
+          {/* <IncorrectNote data={noteData} /> */}아 몰랑 여긴 코드
+        </div>
+        <div className="mentor-chat-content-right">d</div>
+      </div>
     </div>
   );
 }
